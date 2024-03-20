03fe4f1004019ab3d6a51c2a09b38c435d40ae0e
Andrei <garnash1313@gmail.com>
2024-03-20 11:56:46 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Andrei Harnashevich on 20.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
