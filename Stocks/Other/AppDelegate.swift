b51c6ab5881d8f2f714f7b2a0a4f72a3f1e26075
Andrei <garnash1313@gmail.com>
2024-04-02 12:57:53 +0300

    Documentation

--- Stocks/Other/AppDelegate.swift
+++ Stocks/Other/AppDelegate.swift
@@ -10,18 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-//        APICaller.shared.marketData(for: "AAPL", numberOfDays: 1) { result in
-//            switch result {
-//            case .success(let model):
-//                print(model)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
         return true
     }
 
